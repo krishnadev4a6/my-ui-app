2ca98faae6073b977482288cbae74ebb74ad38b8
krishnadev4a6 <krishnathatavarthi4a6@gmail.com>
2024-12-18 14:55:48 +0530

    commit for changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { };
+// next.config.mjs
+export default {
+    productionBrowserSourceMaps: false, // Disable source maps for production
 
-export default nextConfig;
-
+  
+    webpack(config) {
+      // Disable performance hints to optimize build
+      config.performance = {
+        hints: false,
+      };
+      
+      return config;
+    },
+  
+    // Optionally, if you're using environment variables:
+    env: {
+      CUSTOM_API_URL: process.env.CUSTOM_API_URL,
+    },
+  
+    // Additional Next.js optimizations can go here
+  };
+  
